1e5e033abfc363f1056652b181af5188a4ba6340
Davi Cavalcante <davib506@gmail.com>
2025-01-31 00:40:57 -0300

    Develop: Retirando api key do .env e inserindo em arquivo de configuração

--- src/api/transcript.py
+++ src/api/transcript.py
@@ -1,18 +1,14 @@
-from dotenv import load_dotenv
-import os
 import assemblyai as aai
-
-load_dotenv()
+from .config import get_api_key
 
 def start_transcript(config, file_path):
   transcriber = aai.Transcriber(config=config)
   transcript = transcriber.transcribe(file_path)
   return transcript
 
-
 def get_config():
   config = aai.TranscriptionConfig(language_code="pt")
-  aai.settings.api_key = os.getenv('API_KEY')
+  aai.settings.api_key = get_api_key()
   return config
 
 def get_transcription(file_path):
